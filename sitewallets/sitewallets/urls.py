55935260358cfed206cc91d384c1f4e71a9520d2
Михаил Тарбеев <tarb3@yandex.ru>
2025-12-24 08:26:08 +0300

    feat: реализовал авторизацию на основе сессий

--- sitewallets/sitewallets/urls.py
+++ sitewallets/sitewallets/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('wallets.urls')),
+    path('api/v1/drf-auth/', include('rest_framework.urls')),
 ]
