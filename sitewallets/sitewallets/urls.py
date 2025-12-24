62cf4dbbfb14ae624a6f5a7cca4179847a16557e
Михаил Тарбеев <tarb3@yandex.ru>
2025-12-24 10:02:45 +0300

    feat: реализация авторизации по JWT токенам

--- sitewallets/sitewallets/urls.py
+++ sitewallets/sitewallets/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path, re_path
+from rest_framework_simplejwt.views import TokenVerifyView, TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +24,7 @@ urlpatterns = [
     path('api/v1/drf-auth/', include('rest_framework.urls')),
     path(r'api/v1/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
--- sitewallets/wallets/permissions.py
+++ sitewallets/wallets/permissions.py
@@ -3,4 +3,5 @@ from rest_framework import permissions
 
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
+        print('request.user:', request.user, 'obj.user:', obj.user)
         return obj.user == request.user
